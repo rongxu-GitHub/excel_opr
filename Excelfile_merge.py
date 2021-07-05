1a91c82f69dc0f33a1c69690aa5ca6f11e189607
Rick Rong <30464939+rongxu-GitHub@users.noreply.github.com>
2021-07-05 10:48:20 +0800

    Update Excelfile_merge.py

--- Excelfile_merge.py
+++ Excelfile_merge.py
@@ -1,92 +1,74 @@
-"""
-这个程序是用来Excel表格合并的，合并分成两个情况：
-1、不同文件合并；
-2、同一文件内不同sheet表合并
-"""
-
-import os
+import xlrd
+from glob2 import glob
 import pandas as pd
+from pandas import DataFrame
 
+# merge1()使用情况：
+# 适用于合并一个工作簿中的子表
+def merge1():
+    excel_name = r'C:\Users\A\Desktop\WorksheetMerge\file.xlsx'
+    wb = xlrd.open_workbook(excel_name)
 
-def file_merge(file_path_list, str_col):
-    """
-    不同文件薄合并
-    """
-    # 定义一个列表存放读取的数据
-    data_list = []
+    # 获取workbook中所有的表格
+    sheets = wb.sheet_names()
 
-    # 遍历所有文件，进行合并操作
-    for file_path in file_path_list:
-        # 依次读取每个文件，并追加到data_list中，如果有账户ID，记得转换成str类型，header指定第几行作为表头
-        df = pd.read_excel(file_path, converters={str_col: str})
-        data_list.append(df)
+    # 循环遍历所有sheet
+    df_merge = DataFrame()
+    for i in range(len(sheets)):
+        # skiprows=[1],忽略第二行
+        # converters是转换列数据的数据类型的
+        df = pd.read_excel(excel_name, sheet_name=i, index=False, encoding='utf8', converters={'账号ID': str})
+        df_merge = df_merge.append(df)
 
-    # 输出到指定目录下
-    data = pd.concat(data_list)
-    return data
+    df_merge.to_excel(r'C:\Users\A\Desktop\WorksheetMerge\合并.xlsx', sheet_name='合并', index=False)
 
+# merge2()使用情况：
+# 每个表中有相同的子表，且名称相同
+def merge2():
+    # 获取文件夹内所有.xlsx文件
+    file_list = glob(r'C:\Users\A\Desktop\WorksheetMerge' + '\*.xlsx')
+    print(file_list)
+    for name in file_list:
+        print(name.split('\\')[-1].replace('.xlsx', ''))
 
-def sheet_merge(file_path, output_path, str_col):
-    """
-    同文件薄下多个sheet表合并
-    """
-    # 读取文件
-    file = pd.ExcelFile(file_path)
-    # 创建一个列表，用来合并读取的每个sheet表
-    dfs = []
-    # 遍历所有sheet表，添加到dfs[]
-    for sheet_name in file.sheet_names:
-        df = pd.read_excel(file_path, sheet_name=sheet_name, converters={str_col: str})
-        dfs.append(df)
+    FB_merge = DataFrame()
+    GG_merge = DataFrame()
+    for name in file_list:
+        wb = xlrd.open_workbook(name)
+        # 获取workbook中所有的表格
+        sheets = wb.sheet_names()
 
-    # 使用pd.concat()方法拼接成一个DataFrame
-    data = pd.concat(dfs)
-    # 输出到指定文件，index设置不显示
-    data.to_excel(output_path, index=False)
+        for sheet_name in sheets:
+            if sheet_name == "FB":
+                df = pd.read_excel(name, sheet_name=sheet_name, encoding='utf8', converters={'账号ID': str})
+                FB_merge = FB_merge.append(df)
+            else:
+                df = pd.read_excel(name, sheet_name=sheet_name, encoding='utf8', converters={'账号ID': str})
+                GG_merge = GG_merge.append(df)
 
+    # 创建工作簿
+    with pd.ExcelWriter(r'C:\Users\A\Desktop\WorksheetMerge\合并.xlsx') as writer:
+        FB_merge.to_excel(writer, index=False)
+        GG_merge.to_excel(writer, sheet_name='GG', index=False)
+        pass
 
-# 获取目录内的所有文件绝对路径（不包括子目录）
-def get_file_path_list(dir_path):
-    """
-    :param dir_path: 目录
-    :return: 目录内所有文件绝对路径列表（包括子目录中的文件）
-    """
-    # 获取所有文件的绝对路径
-    file_path_list = []
-    for root, dirs, files in os.walk(dir_path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            file_path_list.append(file_path)
-    return file_path_list
+def merge3():
+    # 获取文件夹内所有.xlsx文件
+    file_list = glob(r'C:\Users\A\Desktop\WorksheetMerge' + '\*.xlsx')
+    print(file_list)
+    df_merge = DataFrame()
+    for name in file_list:
+        print(name.split('\\')[-1].replace('.xlsx', ''))
+        df = pd.read_excel(name, encoding='utf8', converters={'账号ID': str})
+        df_merge = df_merge.append(df)
 
-
+    # 创建工作簿
+    with pd.ExcelWriter(r'C:\Users\A\Desktop\WorksheetMerge\合并.xlsx') as writer:
+        df_merge.to_excel(writer, index=False)
+        pass
 
 if __name__ == '__main__':
-    """
-    不同文件合并
-    """
-    # 获取所有要合并的文件的绝对路径
-    dir_path = r'C:\Users\A\Desktop\WorksheetMerge'
-    file_path_list = get_file_path_list(dir_path)
-
-    # 进行合并操作
-    str_col = '帐户编号'
-    data = file_merge(file_path_list, str_col)
-
-    # 输出并指定路径文件名
-    data.to_excel(r'C:\Users\A\Desktop\WorksheetMerge\海外户3月消耗.xlsx', index=False)
-    print('合并完成！')
-
-
-    # """
-    # 同个文件不同sheet表合并
-    # """
-    # # 定义文件绝对路径
-    # file_path = r'C:\Users\A\Desktop\WorksheetMerge\1月-FB广告账户消耗明细-深圳三部阿米巴A组.xlsx'
-    # output_path = r'C:\Users\A\Desktop\WorksheetMerge\合并结果.xlsx'
-    # str_col = '账户ID'
-    # sheet_merge(file_path, output_path, str_col)
-    # print('合并完成！')
-
-
-
+    # 需要哪个l类型的就调用对应的函数
+    merge1()
+    merge2()
+    merge3()
